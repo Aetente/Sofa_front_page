f7246d88f9f77e8f6dda6759c7a745f3dc22cf2a
Aetente <aetente@gmail.com>
2018-02-07 14:17:41 +0200

    added some comments; added highlight chosen marker

--- scripts/main.js
+++ scripts/main.js
@@ -14,10 +14,10 @@ let stepColors= [
     "#50008c"
 ];//colors for steps
 let nowLanguage = "en";//current language, english by default
-let lat = 0;
-let lon = 0;
-let map = null;
-let places = [];
+let lat = 0;//latitude
+let lon = 0;//longitude
+let map = null;//google maps
+let markers = [];//markers on map
 
 let icons = [
     "images/mk_grey.png",
@@ -31,11 +31,11 @@ let icons = [
     "images/mk_step_08.png",
     "images/mk_step_09.png",
     "images/ic_my_location.png"
-];
-let geocoder;
+];//icons for markers
+let geocoder;//with this you decode placeId to get more information
 let weekDay = [
     "mon","tue","wed","thu","fri","sat","sun"
-];
+];//this will be required when you look info about marker
 
 function main(){
     
@@ -43,17 +43,17 @@ function main(){
         $('.sofa-horiz').animate({
             scrollLeft: '-=153'
         }, 1000, 'linear');
-    });//scroll steps
+    });//scroll steps back
     $('.scroll-forward').click(function () {
         $('.sofa-horiz').animate({
             scrollLeft: '+=153'
         }, 1000, 'linear');
-    });//scroll steps
+    });//scroll steps forward
     $('.sofa-search').keypress(
         (e)=>{
             if(e.which==13&&$('.sofa-search').val()){
                 // console.log("https://www.google.com/search?q="+$('.sofa-search').val());
-                window.location.href = "https://www.google.com/search?q="+$('.sofa-search').val();
+                window.location.href = "https://www.google.com/search?q="+$('.sofa-search').val();//google search
             }
         }//search which is located above in right corner
     )
@@ -63,37 +63,38 @@ function main(){
     highlightLanguage(nowLanguage);//highlight choosen language
 }
 
+//set all info
 function setDataByLang(lang){
-    let urlCurr = theUrl+lang;
+    let urlCurr = theUrl+lang;//url to get info by language
     $.ajax({
         url: urlCurr
     }).then(function (data) {
-        // console.log(data);
-        let steps = data.steps;
-        steps.sort((a,b)=>a.numberOfStep-b.numberOfStep);
-        setSelectSteps(steps);
-        setButtonClicks(steps)
-        setInfo(steps,currentStep);
-        setTitleText(lang);
-        highlightLanguage(lang);
-        fillMapWithPlaces(map,lang,currentStep,lat,lon,10);
+        let steps = data.steps;//get info about steps
+        steps.sort((a,b)=>a.numberOfStep-b.numberOfStep);//sort the array of objects, because steps are not in the right order
+        setSelectSteps(steps);//set text for menu where you choose step
+        setButtonClicks(steps)//set clicks in this menu according to the info you got
+        setInfo(steps,currentStep);//set the info about the step
+        setTitleText(lang);//set new title according to language you chose
+        highlightLanguage(lang);//highlight the chosen language
+        fillMapWithPlaces(map,lang,currentStep,lat,lon,10);//fill the map with markers
     });
 }
 
+//set clicks for change language buttons
 function setLanguage(){
-    let qOfLang = $(".changeLang").length;
+    let qOfLang = $(".changeLang").length;//get the quantity of languages
     for(var i=0; i<qOfLang; i++){
-        // console.log( $(".changeLang")[i].getAttribute("id"));
         $(".changeLang")[i].onclick =
             function(){
-                nowLanguage = this.getAttribute("id");
-                setDataByLang(nowLanguage);
+                nowLanguage = this.getAttribute("id");//set the nowLanguage global variable
+                setDataByLang(nowLanguage);//set the data according to langugage you chose
             };
     }
 }
 
+//set clicks in this menu according to the info you got
 function setButtonClicks(steps){
-    let theId = "item1";
+    let theId = "item1";//
     for(var i=0;i<9;i++){
         $("#item"+(i+1)).click(
             function(){
@@ -125,7 +126,6 @@ function setSelectSteps(steps){
 }
 
 function highlightLanguage(nowLanguage){
-    // console.log(nowLanguage);
     let langs = $(".changeLang");
     for(var i=0; i< langs.length; i++){
         langs[i].style.color = "rgba(40,40,40,1)";
@@ -201,18 +201,46 @@ $(window).on( 'resize',
   }
 );
 
-function clearMap(places){
-    if(places.length>0){
-        for(var i=0; i<places.length; i++){
-            places[i].setMap(null);
+function clearMap(){
+    if(markers.length>0){
+        for(var i=0; i<markers.length; i++){
+            markers[i].setMap(null);
         }
-        places=[];
+        markers=[];
+    }
+}
+
+function highlightMarker(n){//highlight the chosen marker
+    for(let i=0; i<markers.length;i++){
+        if(i!=n){//if it is not our marker
+            var icon = {
+                url: icons[0], // url
+                scaledSize: new google.maps.Size(20, 30), // scaled size
+                origin: new google.maps.Point(0,0), // origin
+                anchor: new google.maps.Point(0, 0) // anchor
+            };//new icon(grey one)
+            markers[i].setIcon(icon);//set the grey icon
+        }
+        else{
+        }
+    }
+}
+
+function unhighlightMarkers(){//unhilight all markers
+    for(let i=0; i<markers.length;i++){
+        var icon = {
+            url: icons[currentStep+1], // url
+            scaledSize: new google.maps.Size(20, 30), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+        };//the icon according to current step
+        markers[i].setIcon(icon);//set the icon
     }
 }
 
 function onAddressClick(sofaAddress,markerNumber,place){
     $(".sofa-address").hide(0);
-    console.log(markerNumber);
+    highlightMarker(markerNumber);
     let descriptionHelp = $(".description-help");
     let divToRemove =document.createElement("div");
     divToRemove.className = "div-to-remove";
@@ -273,6 +301,7 @@ function onAddressClick(sofaAddress,markerNumber,place){
 }
 
 function closeCurrentMarker(){
+    unhighlightMarkers();
     $(".div-to-remove").remove();
     $(".sofa-address").show(0);
 }
@@ -280,6 +309,7 @@ function closeCurrentMarker(){
 function fillSofaAddresses(places){
     let descriptionHelp = $(".description-help");
     descriptionHelp.empty();
+    console.log("fillSofaAddresses()")
     console.log(places);
     if(places.length>0){
         for(let i=0; i<places.length; i++){
@@ -319,71 +349,24 @@ function fillSofaAddresses(places){
         }
     }
 }
-
-// function fillSofaAddresses(places){
-//     let descriptionHelp = $(".description-help");
-//     descriptionHelp.empty();
-//     // console.log(places);
-//     if(places.length>0){
-//         for(var i=0; i<places.length; i++){
-//             let sofaAddress = document.createElement("div");
-//             sofaAddress.className = "sofa-address";
-//             descriptionHelp.append(sofaAddress);
-
-//             let imAddress = document.createElement("img");
-//             imAddress.className = "im-address";
-//             sofaAddress.append(imAddress);
-
-//             let addressInfo = document.createElement("div");
-//             addressInfo.className = "address-info";
-//             sofaAddress.append(addressInfo);
-
-//             let nameH5 = document.createElement("h5");
-//             nameH5.append(places[i].name);
-//             sofaAddress.append(nameH5);
-
-//             for(var j=0; j<places[i].phones.length; j++){
-//                 let phoneP = document.createElement("p");
-//                 phoneP.append(places[i].phones[j]);
-//                 sofaAddress.append(phoneP);
-//             }
-
-//             for(var k=0;k<places[i].schedule.length; k++){
-//                 let scheduleP = document.createElement("p");
-//                 scheduleP.append(places[i].schedule[k]);
-//                 sofaAddress.append(scheduleP);
-//             }
-
-//             let urlA = document.createElement("a");
-//             urlA.append(places[i].url);
-//             sofaAddress.append(urlA);
-//         }
-//     }
-// }
-
+//fill the map with markers
 function fillMapWithPlaces(map,lang,step,lat,lon,rad){
-    if(map!=null){
-        // map.clearOverlays();
-        let urlCurr = theUrl+`step/${lang}/${step+1}/area/${lat}/${lon}/${rad}`;
+    if(map!=null){//if the map was initialized
+        let urlCurr = theUrl+`step/${lang}/${step+1}/area/${lat}/${lon}/${rad}`;//url request
+        //TODO change the url to one where you get markers by step
         $.ajax({
             url: urlCurr
         })
-        // fetch(urlCurr)
-        // .then(function (data) {
-        //     console.log("places request successfull");
-        //     return data.json;
-        // })
         .then(function(placesArr){
-           clearMap(places);
-            if(placesArr.length!=0){
+           clearMap();//clear map from markers if threre are already some
+            if(placesArr.length!=0){//if there are any places
                 for(var i=0;i<placesArr.length;i++){
-                    
                     var icon = {
                         url: icons[currentStep+1], // url
                         scaledSize: new google.maps.Size(20, 30), // scaled size
                         origin: new google.maps.Point(0,0), // origin
                         anchor: new google.maps.Point(0, 0) // anchor
-                    };
+                    };//set the icon for marker
                     var marker = new google.maps.Marker({
                         map: map,
                         icon: icon,
@@ -391,10 +374,10 @@ function fillMapWithPlaces(map,lang,step,lat,lon,rad){
                         placeId: placesArr[i].placeId,
                         location: { lat: placesArr[i].latitude, lng: placesArr[i].longitude}
                         }
-                    });
-                    places.push(marker);
+                    });//set marker
+                    markers.push(marker);//push marker to gloabal array so that you could delete them in future or change icons
                 }
-                fillSofaAddresses(placesArr);
+                fillSofaAddresses(placesArr);//fill the div with information about markers
             }
             else{
                 let descriptionHelp = $(".description-help");
@@ -405,6 +388,6 @@ function fillMapWithPlaces(map,lang,step,lat,lon,rad){
     }
 }
 
-function initMap(){
-    navigator.geolocation.getCurrentPosition(successMap, errorMap, options);
+function initMap(){//initialize the google map
+    navigator.geolocation.getCurrentPosition(successMap, errorMap, options);//get location
 }
